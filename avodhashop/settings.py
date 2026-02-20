7471e5dc6b789e36771715989d63e60df934bfe0
muhammedjunaid49 <junaidmuhammed292@gmail.com>
2026-02-20 14:27:09 +0530

    Update settings.py

--- avodhashop/settings.py
+++ avodhashop/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-f5$9p015z0)@tp$ff@@ct61&o%$^lsm(galp(p=nxs#kpq8^d^
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
